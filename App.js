94bf850fa3985a104f97277276bd6849263d860e
SalimSK15 <salimdu15@outook.fr>
2024-01-21 18:59:24 -0500

    ♻️ :  App.js > arranger et factorisation

--- App.js
+++ App.js
@@ -1,20 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Routes from './routes/Routes';
+import { View } from 'react-native';
+import RoutesMenu from './routes/Routes';
 
-export default function App() {
+const App = () => {
   return (
-    <View style={{flex: 1}}>
-      <Routes />
+    <View style={{flex: 1, marginTop: 40}}>
+      <RoutesMenu />
     </View>  
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
