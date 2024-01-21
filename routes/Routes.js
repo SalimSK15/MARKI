70672d1309b312efa8d0e7896a90ec66e3237ad6
SalimSK15 <salimdu15@outook.fr>
2024-01-21 18:17:15 -0500

    ✨ menu_navigation > routes > Routes.js > Routes.js

--- routes/Routes.js
+++ routes/Routes.js
@@ -1,11 +1,19 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { MaterialBottomTabView, createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import MaterialTab from '../composants/Tabs/MaterialTab';
+
+const Tab = createMaterialBottomTabNavigator();
 const Routes = () => {
   return (
-    <View>
-      <Text>Routes</Text>
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator initialRouteName='routeInitial' screenOptions={{headerShown: false}}>
+        <Tab.Screen name="routeInitial" component={MaterialTab} />
+      </Tab.Navigator>
+    </NavigationContainer>
   )
 }
 
