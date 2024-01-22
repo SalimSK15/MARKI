b3d8f9cfcf7966e1db331a010348f68a94c14004
SalimSK15 <salimdu15@outook.fr>
2024-01-21 19:04:40 -0500

    🚧 : menu_navigation > Routes.js > Ajoute de route de menu

--- composants/Tabs/MaterialTab.js
+++ composants/Tabs/MaterialTab.js
@@ -1,14 +1,63 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Home from '../../ecrans/Home/Home';
+import Notifications from '../../ecrans/Notifications/Notifications';
+import RendezVous from '../../ecrans/RendezVous/RendezVous';
+import Profil from '../../ecrans/Profil/Profil';
+
+const Tab = createMaterialBottomTabNavigator();
 
 const MaterialTab = () => {
-  return (
-    <View>
-      <Text>MaterialTab</Text>
-    </View>
-  )
+    return (
+        <Tab.Navigator
+        //   initialRouteName="routeInitial"
+          activeColor={"red"}
+          inactiveColor={"yellow"}
+          barStyle={{ backgroundColor: "white" }}
+        >
+          <Tab.Screen
+            name="Acceuil"
+            component={Home}
+            options={{
+              tabBarLabel: 'Acceuil',
+              tabBarIcon: ({ color }) => (
+                <MaterialCommunityIcons name="home-circle-outline" color={color} size={26} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="Notifications"
+            component={Notifications}
+            options={{
+              tabBarLabel: 'Notification',
+              tabBarIcon: ({ color }) => (
+                <MaterialCommunityIcons name="bell-circle-outline" color={color} size={26} />
+              ),
+            //   tabBarBadge: 3,
+            }}
+          />
+          <Tab.Screen
+            name="Rendez-vous"
+            component={RendezVous}
+            options={{
+              tabBarLabel: 'Rendez-vous',
+              tabBarIcon: ({ color }) => (
+                <MaterialCommunityIcons name="calendar-month" color={color} size={26} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="profil"
+            component={Profil}
+            options={{
+              tabBarLabel: 'Profil',
+              tabBarIcon: ({ color }) => (
+                <MaterialCommunityIcons name="account-convert-outline" color={color} size={26} />
+              ),
+            }}
+          />
+        </Tab.Navigator>
+      );
 }
 
 export default MaterialTab
-
-const styles = StyleSheet.create({})
--- routes/Routes.js
+++ routes/Routes.js
@@ -1,22 +1,18 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
-import { MaterialBottomTabView, createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import MaterialTab from '../composants/Tabs/MaterialTab';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-const Tab = createMaterialBottomTabNavigator();
-const Routes = () => {
+const Stack = createNativeStackNavigator();
+
+const RoutesMenu = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator initialRouteName='routeInitial' screenOptions={{headerShown: false}}>
-        <Tab.Screen name="routeInitial" component={MaterialTab} />
-      </Tab.Navigator>
+      <Stack.Navigator initialRouteName='routeInitial' screenOptions={{headerShown: false}}>
+        <Stack.Screen name="routeInitial" component={MaterialTab} />
+      </Stack.Navigator>
     </NavigationContainer>
   )
 }
 
-export default Routes
-
-const styles = StyleSheet.create({})
+export default RoutesMenu;
