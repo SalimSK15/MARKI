d37e3d99b289481c586f3f58cb1c0323a03dac40
SalimSK15 <salimdu15@outook.fr>
2024-01-29 21:04:47 -0500

    💄 : menu_navigation > Profil > Ajustement du composant profil

--- ecrans/Profil/Profil.js
+++ ecrans/Profil/Profil.js
@@ -1,14 +1,56 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, Image, ScrollView } from 'react-native'
+import { COLORS } from '../../outils/constantes'
+
+import Btn from '../../composants/Button/Btn'
+
+// import { generateFakeData } from '../../assets/fakedata/fakeData'
+import Text_Input from '../../composants/Text_Input/Text_Input'
 
 const Profil = () => {
   return (
-    <View>
-      <Text>Profil</Text>
+    <View style={styles.containerProfil}>
+      <View style={styles.photo_profil}>
+          <Image source={require("../../assets/images/photo_Profil_vide_x2.png")} />
+      </View>
+      <Btn style={styles.btnSeConnecter} textBtn={"Modifier la photo de profil"} />
+      <ScrollView >
+        <Text_Input style={styles.style_Text_Input} text_label={"Nom"} text_placeholder={"Nom"} />
+        <Text_Input text_label={"Prénom"} text_placeholder={"Prénom"} />
+        <Text_Input text_label={"Année de naissance"} text_placeholder={"Année de naissance"} />
+        <Text_Input text_label={"Email"} text_placeholder={"Ex : nom@gmail.com"} />
+        <Text_Input text_label={"Numéro de téléphone"} text_placeholder={"Ex : 05 51 12 3x xx"} />
+      </ScrollView>
     </View>
   )
 }
 
-export default Profil
+const styles = StyleSheet.create({
+  containerProfil:{
+    flex: 1,
+    padding: 15,
+    backgroundColor: COLORS.White,
+  },
+  photo_profil:{
+    borderWidth: 2,
+    width: 200,
+    height: 200,
+    borderRadius: 15,
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 10
+  },
+  btnSeConnecter:{
+    padding: 10,
+    color: 'white',
+    borderRadius: 8,
+    backgroundColor: COLORS.VioletFonce,
+    fontSize: 15,
+    textAlign: "center",
+    width: 200,
+  },
+  style_Text_Input:{
+    // height: 50,
+  }
+})
 
-const styles = StyleSheet.create({})
+export default Profil;
