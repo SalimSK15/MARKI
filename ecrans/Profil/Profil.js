ef885e34e0df52b273eb5d91f1b5c2ef50d03eaa
SalimSK15 <salimdu15@outook.fr>
2024-02-11 01:05:41 -0500

    💄 : Profil > ajout de style_Text_input

--- ecrans/Profil/Profil.js
+++ ecrans/Profil/Profil.js
@@ -1,9 +1,8 @@
-import { StyleSheet, Text, View, Image, ScrollView } from 'react-native'
+import { StyleSheet, View, Image, ScrollView } from 'react-native'
 import { COLORS } from '../../outils/constantes'
 
 import Btn from '../../composants/Button/Btn'
 
-// import { generateFakeData } from '../../assets/fakedata/fakeData'
 import Text_Input from '../../composants/Text_Input/Text_Input'
 
 const Profil = () => {
@@ -15,10 +14,10 @@ const Profil = () => {
       <Btn style={styles.btnSeConnecter} textBtn={"Modifier la photo de profil"} />
       <ScrollView >
         <Text_Input style={styles.style_Text_Input} text_label={"Nom"} text_placeholder={"Nom"} />
-        <Text_Input text_label={"Prénom"} text_placeholder={"Prénom"} />
-        <Text_Input text_label={"Année de naissance"} text_placeholder={"Année de naissance"} />
-        <Text_Input text_label={"Email"} text_placeholder={"Ex : nom@gmail.com"} />
-        <Text_Input text_label={"Numéro de téléphone"} text_placeholder={"Ex : 05 51 12 3x xx"} />
+        <Text_Input style={styles.style_Text_Input} text_label={"Prénom"} text_placeholder={"Prénom"} />
+        <Text_Input style={styles.style_Text_Input} text_label={"Année de naissance"} text_placeholder={"Année de naissance"} />
+        <Text_Input style={styles.style_Text_Input} text_label={"Email"} text_placeholder={"Ex : nom@gmail.com"} />
+        <Text_Input style={styles.style_Text_Input} text_label={"Numéro de téléphone"} text_placeholder={"Ex : 05 51 12 3x xx"} />
       </ScrollView>
     </View>
   )
@@ -32,24 +31,30 @@ const styles = StyleSheet.create({
   },
   photo_profil:{
     borderWidth: 2,
-    width: 200,
-    height: 200,
+    width: "55%",
+    height: 200,  
     borderRadius: 15,
     alignItems: "center",
     justifyContent: "center",
     marginBottom: 10
   },
   btnSeConnecter:{
+    width: "55%",
     padding: 10,
+    fontSize: 15,
     color: 'white',
     borderRadius: 8,
-    backgroundColor: COLORS.VioletFonce,
-    fontSize: 15,
     textAlign: "center",
-    width: 200,
+    backgroundColor: COLORS.VioletFonce,
   },
   style_Text_Input:{
-    // height: 50,
+    height: 40,
+    borderWidth: 1,
+    paddingLeft: 15,
+    borderTopRightRadius: 10,
+    borderBottomRightRadius: 10,
+    borderBottomLeftRadius: 5,
+    borderTopLeftRadius: 5,
   }
 })
 
