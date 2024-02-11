954fb0f47df69fe225f8dff7bffa76502bcfde93
SalimSK15 <salimdu15@outook.fr>
2024-02-11 17:05:28 -0500

    💄 : Ecran_Confirmation > Interface_Confirmation > ajout du style et du contenu

--- composants/Ecran_Confirmation/Interface_Confirmation.js
+++ composants/Ecran_Confirmation/Interface_Confirmation.js
@@ -1,15 +1,57 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import Btn from '../Button/Btn';
+import { COLORS } from '../../outils/constantes';
+import Text_Input from '../../composants/Text_Input/Text_Input'
 
-const Interface_Confirmation = () => {
+const Interface_Confirmation = (props) => {
+
+  const handlePressConfirmation = () =>{
+    props.navigation.navigate('Home');
+  }
+
   return (
-    <View>
-      <Text>Interface_Confirmation</Text>
+    <View style={styles.container}>
+      <View style={styles.sousContenue}>
+        <Text style={styles.contenuText}>Saisissez le code envoyer à votre téléphone</Text>
+        <Text_Input style={styles.style_Text_Input} text_placeholder={"*******"} />
+        <Btn textBtn={"Confirmer"} style={styles.btnConfirmer} fonction={handlePressConfirmation}/>
+      </View>
     </View>
   )
 }
 
-
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    padding: 20,
+    backgroundColor: COLORS.White,
+  },
+  sousContenue:{
+    marginTop: "30%"
+  },
+  contenuText:{
+    fontSize: 16,
+    fontWeight: "bold",
+    textAlign: "center",
+    // marginTop: -10
+  },
+  style_Text_Input:{
+    height: 45,
+    borderWidth: 1,
+    paddingLeft: 15,
+    borderRadius: 20,
+  },
+  btnConfirmer:{
+    marginTop: 50,
+    padding: 10,
+    fontSize: 20,
+    color: 'white',
+    paddingLeft: 20,
+    paddingRight: 20,
+    borderRadius: 5,
+    textAlign: "center",
+    backgroundColor: COLORS.VioletFonce,
+  }
+})
 
 export default Interface_Confirmation;
