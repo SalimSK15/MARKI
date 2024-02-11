feea97f6a9b560b74d57bb7a4c7f8fe43bf2427d
SalimSK15 <salimdu15@outook.fr>
2024-02-11 01:37:06 -0500

    ✨ : Ecran_Confirmation > Interface_Confirmation > Ajout du composant

---
+++ composants/Ecran_Confirmation/Interface_Confirmation.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Interface_Confirmation = () => {
+  return (
+    <View>
+      <Text>Interface_Confirmation</Text>
+    </View>
+  )
+}
+
+
+const styles = StyleSheet.create({})
+
+export default Interface_Confirmation;
